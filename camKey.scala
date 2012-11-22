8dbc54d301d2218f4738b4bc646e2950cd8ba39b
HairyFotr <hairyfotr@gmail.com>
2012-11-22 01:44:33 +0100

    added camMouse

--- camKey.scala
+++ camKey.scala
@@ -31,16 +31,19 @@ object camKey extends App {
         } catch { 
             case e:Throwable => 
             e.printStackTrace
+            println()
+            println("Is the camera on... did you follow the README?")
             sys.exit(-1)
             None 
         }
         
-    val cam = getCamera(0).get
+    val cam = getCamera(try { args(0).toInt } catch { case e:Exception => 0 }).get
     
+    val imgRange = (0 until width*height)
     def captureFrame(pixels:Array[Array[Int]]) = {
         Option(cam.grab) foreach { img =>
             val imgData = img.getBufferedImage.getData.getDataBuffer.asInstanceOf[java.awt.image.DataBufferByte]
-            for(i <- 0 until width*height) 
+            for(i <- imgRange) 
                 pixels(i%width)(i/width) = (
                     imgData.getElem(i*3+0).toInt +
                     imgData.getElem(i*3+1).toInt +
@@ -51,85 +54,95 @@ object camKey extends App {
 
     val robot = new Robot
     var avgtime = 0d
-    var ex, ey = 0
+    var ex = 0
     var pic1 = Array.ofDim[Int](width, height)
     var pic2 = Array.ofDim[Int](width, height)
     captureFrame(pic1)
     captureFrame(pic2)
     var tick = now
     while(true) {
-        var gradL1, gradR1, gradL2, gradR2, gradL3, gradR3, 
-            gradDL, gradDR, gradUL, gradUR,
-            gradU1, gradD1, gradU2, gradD2, gradU3, gradD3, gradC = 0
-        val edge = 3
+        val edge = 17
+        
+        var gradL1, gradR1, gradL2, gradR2, gradL3, gradR3, gradL4, gradR4, gradL5, gradR5, gradL6, gradR6, gradL7, gradR7,
+            gradDL, gradDR, gradUL, gradUR, gradDL2, gradDR2, gradUL2, gradUR2,
+            gradC = 0
         for(i <- edge until width-edge; j <- edge until height-edge) {
-            gradC  += abs(pic1(i)(j) - pic2(i)(j))
             gradL1 += abs(pic1(i)(j) - pic2(i-1)(j))
             gradL2 += abs(pic1(i)(j) - pic2(i-2)(j))
             gradL3 += abs(pic1(i)(j) - pic2(i-3)(j))
+            gradL4 += abs(pic1(i)(j) - pic2(i-4)(j))
+            gradL5 += abs(pic1(i)(j) - pic2(i-7)(j))
+            gradL6 += abs(pic1(i)(j) - pic2(i-11)(j))
+            gradL7 += abs(pic1(i)(j) - pic2(i-17)(j))
+            gradC  += abs(pic1(i)(j) - pic2(i)(j))
             gradR1 += abs(pic1(i)(j) - pic2(i+1)(j))
             gradR2 += abs(pic1(i)(j) - pic2(i+2)(j))
             gradR3 += abs(pic1(i)(j) - pic2(i+3)(j))
-            gradUL += abs(pic1(i)(j) - pic2(i-1)(j-1))
-            gradUR += abs(pic1(i)(j) - pic2(i+1)(j-1))
-            gradDL += abs(pic1(i)(j) - pic2(i-1)(j+1))
-            gradDR += abs(pic1(i)(j) - pic2(i+1)(j+1))
-            gradU1 += abs(pic1(i)(j) - pic2(i)(j-1))
-            gradU2 += abs(pic1(i)(j) - pic2(i)(j-2))
-            gradU3 += abs(pic1(i)(j) - pic2(i)(j-3))
-            gradD1 += abs(pic1(i)(j) - pic2(i)(j+1))
-            gradD2 += abs(pic1(i)(j) - pic2(i)(j+2))
-            gradD3 += abs(pic1(i)(j) - pic2(i)(j+3))
+            gradR4 += abs(pic1(i)(j) - pic2(i+4)(j))
+            gradR5 += abs(pic1(i)(j) - pic2(i+7)(j))
+            gradR6 += abs(pic1(i)(j) - pic2(i+11)(j))
+            gradR7 += abs(pic1(i)(j) - pic2(i+17)(j))
+            gradUL += abs(pic1(i)(j) - pic2(i-2)(j-1))
+            gradUR += abs(pic1(i)(j) - pic2(i+2)(j-1))
+            gradDL += abs(pic1(i)(j) - pic2(i-2)(j+1))
+            gradDR += abs(pic1(i)(j) - pic2(i+2)(j+1))
+            gradUL2 += abs(pic1(i)(j) - pic2(i-5)(j-2))
+            gradUR2 += abs(pic1(i)(j) - pic2(i+5)(j-2))
+            gradDL2 += abs(pic1(i)(j) - pic2(i-5)(j+2))
+            gradDR2 += abs(pic1(i)(j) - pic2(i+5)(j+2))
         }
-        var gradsx = List(gradL1, gradR1, gradL2, gradR2, gradL3, gradR3, gradUL, gradUR, gradDL, gradDR, gradC).zipWithIndex.minBy(_._1)._2
+        var gradsx = List(gradC, gradUL, gradUR, gradDL, gradDR, 
+            gradL1, gradR1, gradL2, gradR2, gradL3, gradR3, gradL4, gradR4, 
+            gradL5, gradR5, gradL6, gradR6, gradL7, gradR7,
+            gradUL, gradUR, gradDL, gradDR
+        ).zipWithIndex.minBy(_._1)._2
+        
         var x = -(gradsx match {
-            case 0 => -1
-            case 1 => +1
-            case 2 => -2
-            case 3 => +2
-            case 4 => -3
-            case 5 => +3
-            case 6 => -2
-            case 7 => +2
-            case 8 => -2
-            case 9 => +2
-            case 10=> 0
-        })
-        var gradsy = List(gradU1, gradD1, gradU2, gradD2, gradU3, gradD3, gradUL, gradUR, gradDL, gradDR, gradC).zipWithIndex.minBy(_._1)._2
-        var y = gradsy match {
-            case 0 => -1
-            case 1 => +1
-            case 2 => -2
-            case 3 => +2
-            case 4 => -3
-            case 5 => +3
-            case 6 => -2
+            case 0 =>  0
+            case 1 => -3
+            case 2 => +3
+            case 3 => -3
+            case 4 => +3
+            case 5 => -1
+            case 6 => +1
             case 7 => -2
             case 8 => +2
-            case 9 => +2
-            case 10=> 0
-        }
+            case 9 => -3
+            case 10=> +3
+            case 11=> -4
+            case 12=> +4
+            case 13=> -7
+            case 14=> +7
+            case 15=> -11
+            case 16=> +11
+            case 17=> -18
+            case 18=> +18
+            case 19=> -6
+            case 20=> +6
+            case 21=> -6
+            case 22=> +6
+        })        
         
         val p = MouseInfo.getPointerInfo.getLocation
-        if(x >= 2 || (ex >= 1 && x >= 1)) {
+        if(x >= 6 || (ex >= 4 && x >= 2) || (ex >= 3 && x >= 1)) {
             robot.keyPress(KeyEvent.VK_RIGHT)
-            robot.mouseMove(p.x+x*3, p.y)
-        } else if(x <= -2 || (ex <= -1 && x <= -1)) {
+            //robot.mouseMove(p.x+x*5, p.y)
+        } else if(x <= -6 || (ex <= -4 && x <= -2) || (ex <= -3 && x <= -1)) {
             robot.keyPress(KeyEvent.VK_LEFT)
-            robot.mouseMove(p.x+x*3, p.y)
+            //robot.mouseMove(p.x+x*5, p.y)
         } else {
             robot.keyRelease(KeyEvent.VK_RIGHT)
             robot.keyRelease(KeyEvent.VK_LEFT)
         }
 
-        ex = x
-        ey = y
+        ex = (ex + x)/2
+
         val swap = pic1
         pic1 = pic2
         pic2 = swap
         captureFrame(pic2)
         
-        print((x,y) + "                 \t\t\t " + ("%1.2f" format avgtime) + "           \r")
+        print("\r" + x + "                 \t\t\t " + ("%1.2f" format avgtime) + "           \r")
         avgtime = (since(tick)*0.2d + avgtime*0.8d)
         tick = now
     }
---
+++ camMouse.scala
@@ -0,0 +1,166 @@
+import com.googlecode.javacv._
+import com.googlecode.javacv.cpp.opencv_core._
+import com.googlecode.javacv.cpp.opencv_highgui._
+import util.Random._
+import Thread.sleep
+import math._
+import collection.mutable.{HashMap,HashSet,ListBuffer,LinkedHashMap}
+import java.awt.{MouseInfo,Robot}
+import java.awt.event.KeyEvent
+import scala.concurrent._
+import scala.concurrent.util._
+import scala.concurrent.duration._
+import ExecutionContext.Implicits.global
+
+object camMouse extends App {
+    var timeDivisor = 1000000L
+    def since(time: Int): Int = now-time
+    def now = (System.nanoTime()/timeDivisor).toInt
+    def time(func: => Unit) = {
+        val startTime = now
+        func
+        now-startTime
+    }
+    val (inf,ninf) = (Double.PositiveInfinity, Double.NegativeInfinity)
+    var width = 640
+    var height = 480
+
+    // init
+    def getCamera(i:Int):Option[FrameGrabber] = 
+        try { 
+            val cam = new OpenCVFrameGrabber(i) 
+            cam.setImageWidth(width)
+            cam.setImageHeight(height)
+            cam.start
+            Some(cam)
+        } catch { 
+            case e:Throwable => 
+            e.printStackTrace
+            println()
+            println("Is the camera on... did you follow the README?")
+            sys.exit(-1)
+            None 
+        }
+        
+    val camID = try { args(0).toInt } catch { case e:Exception => 0 }
+    val cam = getCamera(camID).get
+    
+    val imgRange = (0 until width*height)
+    def captureFrame(pixels:Array[Array[Int]]) = {
+        Option(cam.grab) foreach { img =>
+            val imgData = img.getBufferedImage.getData.getDataBuffer.asInstanceOf[java.awt.image.DataBufferByte]
+            for(i <- imgRange) 
+                pixels(i%width)(i/width) = (
+                    imgData.getElem(i*3+0).toInt +
+                    imgData.getElem(i*3+1).toInt +
+                    imgData.getElem(i*3+2).toInt
+                )
+        }
+    }
+
+    var robot = new Robot
+    var avgTime = 0d
+    var pic1 = Array.ofDim[Int](width, height)
+    var pic2 = Array.ofDim[Int](width, height)
+    captureFrame(pic1)
+    captureFrame(pic2)
+    var tick = now
+    val edge = 25
+    var ex, ey = 0d
+    val randPoints = Array.tabulate(77)(a=> (nextInt(width-(edge+1)*2)+edge+1, nextInt(height-(edge+1)*2)+edge+1)).distinct.par
+    var cnt = 0
+    var x0,y0 = 0d
+    var xt1,xt2,yt1,yt2,xt,yt = 0d
+    val init = 50
+    var noise = List[(Double,Double)]()
+    var futurepic = future {}
+    val Arange = (0 until 7).toArray
+    println("Be still for a few seconds :)")
+    while(true) {
+        Await.result(futurepic, Duration.Inf)
+        var vectors = for((x,y) <- randPoints) yield {
+            def getArea1(x:Int,y:Int,pic:Array[Array[Int]]) = Array[Int](
+                 pic(x)(y)*2,
+                 pic(x+1)(y),
+                 pic(x-1)(y),
+                 pic(x)(y+1),
+                 pic(x)(y-1),
+                 pic(x+1)(y+1)/2,
+                 pic(x-1)(y+1)/2,
+                 pic(x+1)(y-1)/2,
+                 pic(x-1)(y-1)/2
+            )
+            val exP = getArea1(x,y, pic1)
+            def getArea(x:Int,y:Int,pic:Array[Array[Int]]) = {
+                val cP = getArea1(x,y,pic)
+                Arange.foldLeft(0)((score, i) => score + abs(exP(i)-cP(i)))
+            }
+                
+            var minDist = inf
+            var minP = (0d,0d)
+            for(i <- -edge until edge; j <- -edge until edge) {
+                val currDist = getArea(x+i,y+j, pic2) + sqrt(math.pow(i, 2) + math.pow(j, 2))*7
+                if(currDist < minDist) {
+                    minDist = currDist
+                    minP = (i,j)
+                }
+            }
+            //if(minP != (0,0)) 
+            minP
+        }
+        futurepic = future {
+            val swap = pic1
+            pic1 = pic2
+            pic2 = swap
+            captureFrame(pic2)
+        }
+        
+        val (x,y) = {
+            var out = vectors.filterNot(_==(0d,0d)).foldLeft((0d,0d))((acc, v) => (acc._1+v._1, acc._2+v._2))
+            out = (out._1/vectors.size*avgTime*1.3, out._2/vectors.size*(avgTime*3))
+            if(cnt <= init) 
+                (-out._1, out._2)
+            else
+                (-out._1-x0, out._2-y0)
+        }
+        
+        
+        ex = (ex + x)/2
+        ey = (ey + y)/2
+        if(cnt < init) {
+            noise = noise :+ (x,y)            
+        } else if(cnt == init) {
+            def noiseAvg(m:((Double,Double))=>Double)(f:Double=>Boolean) = { 
+                val n = noise.map(m).filter(f)
+                n.sum/n.size
+            }
+            
+            x0 = noiseAvg(_._1)(a=>true)
+            y0 = noiseAvg(_._2)(a=>true)
+            xt1 = (noiseAvg(_._1)(_ < x0)-x0)
+            xt2 = (noiseAvg(_._1)(_ > x0)-x0)
+            yt1 = (noiseAvg(_._2)(_ < y0)-y0)
+            yt2 = (noiseAvg(_._2)(_ > y0)-y0)
+            xt = min(abs(xt1),abs(xt2))
+            yt = min(abs(yt1),abs(yt2))
+            println(List(x0,y0,xt1,xt2,yt1,yt2).map(_.toInt).mkString(","))
+            println("Move now :)")
+        } else {
+            val p = MouseInfo.getPointerInfo.getLocation
+            val (vx,vy) = (
+                if(ex <= xt1/2 || ex >= xt2/2) (ex - signum(ex)*xt).toInt else 0,
+                if(ey <= yt1/2 || ey >= yt2/2) (ey - signum(ey)*yt).toInt else 0
+                //ex.toInt,ey.toInt
+            )
+            robot.mouseMove(p.x+vx, p.y+vy)
+        }
+
+        cnt += 1
+        print("\r" + (x.toInt,y.toInt) + "                 \t\t\t " + ("%1.2f" format avgTime) + "           \r")
+        avgTime = (since(tick)*0.2d + avgTime*0.8d)
+        tick = now
+    }
+    
+    // cleanup
+    cam.stop
+}
